5609ed805be56ace5d6589134714e4bdabc372ed
Suyash Verma <suyashvermaabes27@gmail.com>
2026-04-24 14:20:00 +0530

    Add files via upload

---
+++ Lecture7.js
@@ -0,0 +1,10 @@
+// using the debugger to find the error
+// go to run find run debugging 
+// then start running your code 
+// add the breakpoint and execute ine by line 
+// for example 
+let a=500;
+while(a>0){
+  console.log(a);
+  a--;
+};
