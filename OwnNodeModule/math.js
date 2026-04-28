ab82e172ff9e4b22c65bf660280fde5fc4568ac4
Suyash Verma <suyashvermaabes27@gmail.com>
2026-04-28 17:01:55 +0530

    Add files via upload

---
+++ OwnNodeModule/math.js
@@ -0,0 +1,13 @@
+let add=(a,b)=>{
+  c=parseInt(a+b);
+  return c;
+}
+let substract=(a,b)=>{
+  c=parseInt(a-b);
+  return c;
+}
+let divide=(a,b)=>{
+  c=parseInt(a/b);
+  return c;
+}
+module.exports={add,substract,divide};
